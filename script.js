2c06a16394036818486a08d7c2e16c883d079730
Martina Thege <martinathege@gmail.com>
2022-05-16 13:06:52 +0200

    Martina test

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 
 class Icecream {
-  constructor(name, flavor) {
-    (this.name = name), (this.flavor = flavor);
-  }
+    constructor(name, flavor) {
+        (this.name = name), (this.flavor = flavor);
+    }
 }
 
 const add = (num1, num2) => num1 + num2;
@@ -11,26 +11,45 @@ const add = (num1, num2) => num1 + num2;
 //Tillägg - Nike
 const sum = (sum1, sum2) => sum1 + sum2;
 const numbers = {
-  isNull: () => null
+    isNull: () => null
 }
 
 function addToShoppingList(shoppingList, newItem) {
-  shoppingList.push(newItem);
-  return shoppingList;
+    shoppingList.push(newItem);
+    return shoppingList;
 }
 
-const fetchData = async (id) => {
-  let result;
-  try {
-    result = await axios.get(
-      `https://jsonplaceholder.typicode.com/users/${id}`
-    );
-    return result.data;
-  } catch (err) {
-    return null;
-  }
+const fetchData = async(id) => {
+    let result;
+    try {
+        result = await axios.get(
+            `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        return result.data;
+    } catch (err) {
+        return null;
+    }
 };
 
 console.log(fetchData(11));
 
-module.exports = { Icecream, add, sum, numbers, addToShoppingList, fetchData };
+//Martina
+
+function toRovarSprak(namn) {
+
+    let vokaler = ["a", "e", "i", "o", "u", "y", "å", "ä", "ö", "-"];
+    let resultat = "";
+
+    for (let i = 0; i < namn.length; i++) {
+
+        if (!vokaler.includes(namn[i].toLowerCase())) {
+            resultat += namn[i] + "o" + namn[i].toLowerCase();
+        } else {
+            resultat += namn[i];
+        }
+    }
+
+    return resultat;
+}
+
+module.exports = { Icecream, add, sum, numbers, addToShoppingList, fetchData, toRovarSprak };
--- script.test.js
+++ script.test.js
@@ -1,49 +1,63 @@
 const { default: axios } = require('axios');
-const { Icecream, add, sum, numbers, addToShoppingList, fetchData } = require('./script');
+const { Icecream, add, sum, numbers, addToShoppingList, fetchData, toRovarSprak } = require('./script');
 
 describe('Testing Icecream-class', () => {
-  test('returns a object from class', () => {
-    const lakritsPuck = new Icecream('puck', 'lakrits');
-    expect(lakritsPuck).toEqual({
-      flavor: 'lakrits',
-      name: 'puck',
+    test('returns a object from class', () => {
+        const lakritsPuck = new Icecream('puck', 'lakrits');
+        expect(lakritsPuck).toEqual({
+            flavor: 'lakrits',
+            name: 'puck',
+        });
     });
-  });
 });
 
 test('Adds 10+10 to NOT equal 50', () => {
-  expect(add(10, 10)).not.toBe(50);
+    expect(add(10, 10)).not.toBe(50);
 });
 
 //Tillägg - Nike
-describe ('Testing to add and compare numbers', () => {
-  test('Should add 5 + 8 to equal 13', () => {
-    const result = sum(5,8);
-    expect(result).toBe(13);
-    expect(result).toBeLessThan(25);
-  });
+describe('Testing to add and compare numbers', () => {
+    test('Should add 5 + 8 to equal 13', () => {
+        const result = sum(5, 8);
+        expect(result).toBe(13);
+        expect(result).toBeLessThan(25);
+    });
 });
 
 test('Should be null', () => {
-  expect(numbers.isNull()).toBeNull();
+    expect(numbers.isNull()).toBeNull();
 });
 
+//Tar bort då exemplet är likt det i dokumentationen. 
+describe.skip('Handle shopping list', () => {
+    const shoppingList = ['bread', 'milk', 'butter'];
+    test.skip('add rice to shopping list', () => {
+        expect(addToShoppingList(shoppingList, 'rice')).toContain('rice');
+    });
+});
 
-describe('Handle shopping list', () => {
-  const shoppingList = ['bread', 'milk', 'butter'];
-  test('add rice to shopping list', () => {
-    expect(addToShoppingList(shoppingList, 'rice')).toContain('rice');
-  });
+//Martina
+
+describe('Lets test rovarspraket', () => {
+    test('Translate with capital letter', () => {
+        expect(toRovarSprak("Martina")).toBe("Momarortotinona");
+    });
+    test('Translate without capital letter', () => {
+        expect(toRovarSprak("erik")).toBe("erorikok");
+    });
+    test('Translate with special characters', () => {
+        expect(toRovarSprak("Björn-Olav")).toBe("Bobjojörornon-Ololavov");
+    });
 });
 
 describe('Testing api-requests', () => {
-  test('Should return user', async () => {
-    const user = await fetchData(1);
-    expect(user.id).toBe(1);
-  });
-  test('Should return error', async () => {
-    const user = await fetchData(11);
-    console.log(user);
-    expect(user).toBeNull();
-  });
+    test('Should return user', async() => {
+        const user = await fetchData(1);
+        expect(user.id).toBe(1);
+    });
+    test('Should return error', async() => {
+        const user = await fetchData(11);
+        console.log(user);
+        expect(user).toBeNull();
+    });
 });
