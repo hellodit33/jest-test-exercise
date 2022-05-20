9eb2ca392db611eab535af36ef85f710b95175e7
hellodit33 <45536957+hellodit33@users.noreply.github.com>
2022-05-20 10:01:06 +0200

    2 tests elodie

---
+++ script.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+
+class Icecream {
+  constructor(name, flavor, price) {
+    (this.name = name), (this.flavor = flavor), (this.price = price);
+  }
+  updatePrice(newPrice) {
+    this.price = newPrice;
+  }
+}
+
+const add = (num1, num2) => num1 + num2;
+
+//Tillägg - Nike
+const sum = (sum1, sum2) => sum1 + sum2;
+const numbers = {
+  isNull: () => null,
+};
+
+function addToShoppingList(shoppingList, newItem) {
+  shoppingList.push(newItem);
+  return shoppingList;
+}
+
+function removeFormShoppingList(shoppingList, item) {
+  return shoppingList.filter((itemInCart) => itemInCart !== item);
+}
+
+const fetchData = async (id) => {
+  let result;
+  try {
+    result = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return result.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+//Martina
+
+function toRovarSprak(namn) {
+  let vokaler = ['a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö', '-'];
+  let resultat = '';
+
+  for (let i = 0; i < namn.length; i++) {
+    if (!vokaler.includes(namn[i].toLowerCase())) {
+      resultat += namn[i] + 'o' + namn[i].toLowerCase();
+    } else {
+      resultat += namn[i];
+    }
+  }
+
+  return resultat;
+}
+
+//Elodie
+function cloneArray(array) {
+  return [...array]
+}
+
+let programmingLanguages = ['python', 'php', 'javascript', 'c#', 'R', 'swift']
+
+
+module.exports = {
+  Icecream,
+  add,
+  sum,
+  numbers,
+  addToShoppingList,
+  removeFormShoppingList,
+  fetchData,
+  toRovarSprak,
+  cloneArray,
+  programmingLanguages
+};
